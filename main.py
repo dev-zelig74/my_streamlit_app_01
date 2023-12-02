0859ab7477c9bb583022e6a7e9439e6809f24775
dev-zelig74 <152331786+dev-zelig74@users.noreply.github.com>
2023-12-02 21:34:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
