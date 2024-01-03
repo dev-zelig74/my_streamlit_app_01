361e567d338a95c6cea3506594664e197058db4b
Okay Cakir <152331786+dev-zelig74@users.noreply.github.com>
2024-01-03 22:52:03 +0300

    Update st.selectbox-exercise.py

--- st.selectbox-exercise.py
+++ st.selectbox-exercise.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header('st.selectbox')
 
 option = st.selectbox(
-    'What is your favorite color?'
+    'What is your favorite color?',
     ('Blue', 'Red', 'Green'))
 
 st.write('Your favorite color is ', option)
