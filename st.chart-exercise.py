e246def289ca7965f27993bf96f4a1c387b21e1c
Okay Cakir <152331786+dev-zelig74@users.noreply.github.com>
2024-01-03 22:41:51 +0300

    Update st.chart-exercise.py

--- st.chart-exercise.py
+++ st.chart-exercise.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altait as alt
 
 st.header('Line chart')
 
@@ -9,3 +10,13 @@ chart_data = pd.DataFrame(
     columns = ['a','b','c'])
 
 st.line_chart(chart_data)
+
+st.subheader('Altair line chart')
+
+c = (
+   alt.Chart(chart_data)
+   .mark_circle()
+   .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.altair_chart(c, use_container_width=True)
